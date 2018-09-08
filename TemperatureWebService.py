6b0b9dc1aac2919f8693613750043ac12a61ceb2
jsulliv2 <jsulliv2@gmail.com>
2018-09-07 23:31:10 -0400

    Update To Convert Methods
    Switching from seperate functions for each temp imput to conversion from any temp to kelvin, then convert k to other temps, pop the unneeded unit.

--- TemperatureWebService.py
+++ TemperatureWebService.py
@@ -7,62 +7,52 @@
 '''
 from flask import Flask
 from json import JSONEncoder
+import sys
 
 app = Flask(__name__)
 
-
-def fahrenheit(fahr):
-    returnTemp = {}
-    returnTemp['celsius'] = (fahr-32)/1.8
-    returnTemp['kelvin'] = (fahr + 459.67)*5/9
-    returnTemp['rankine'] = fahr + 459.67
-    return returnTemp
-
-
-def celsius(cels):
-    returnTemp = {}
-    returnTemp['fahrenheit'] = cels*9/5+32
-    returnTemp['kelvin'] = cels+273.15
-    returnTemp['rankine'] = (cels+273.15)*9/5
-    return returnTemp
-
-
+#take kelvin temp and create dict containing values for all temps
 def kelvin(k):
     returnTemp = {}
+    returnTemp['kelvin'] = k
     returnTemp['fahrenheit'] = k*9/5-459.67
     returnTemp['celsius'] = k-273.15
     returnTemp['rankine'] = k*9/5
     return returnTemp
 
-
-def rankine(ran):
-    returnTemp = {}
-    returnTemp['fahrenheit'] = ran-459.67
-    returnTemp['celsius'] = (ran - 491.67)*5/9
-    returnTemp['kelvin'] = ran*5/9
-    return returnTemp
-
+#turn temp into kelvin, check for absolute zero
+def translate(unit, temp):
+    toKelv = {'fahrenheit': (temp+459.67)*5/9, 'celsius': temp+273.15, 'rankine': temp*5/9, 'kelvin': temp}
+    temp = toKelv[unit]
+    if temp < 0:
+        raise Exception('Entered number is below absolute zero')
+    return temp
 
 @app.route('/convert/<unit>/<temp>/')
 def main(unit, temp):
-    results = {}
     unit = unit.lower()
-    absoluteZero = {'kelvin': 0, 'rankine': 0, 'celsius': -273.15, 'fahrenheit': -459.67}
+    if unit not in ('kelvin', 'celsius', 'rankine', 'fahrenheit'):
+        raise Exception('Unsupported unit entered, please use kelvin, fahrenheit, celsius, or rankine.')
+    results = {}
     try:
         temp = float(temp.replace(",", "").replace(" ", ""))
     except Exception as e:
         raise e('Error turning temp input into a number')
-    if temp < absoluteZero[unit]:
-        raise Exception('Value provided is below absoluteZero')
-    if unit == 'fahrenheit':
-        results = fahrenheit(temp)
-    elif unit == 'celsius':
-        results = celsius(temp)
-    elif unit == 'rankine':
-        results = rankine(temp)
-    elif unit == 'kelvin':
-        results = kelvin(temp)
-    if results.keys():
+    temp = translate(unit, temp)
+    results = kelvin(temp)
+    results.pop(unit)
+    if results:
         return JSONEncoder().encode(results)
     else:
         raise Exception("Return object empty.")
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        debug = sys.argv[1]
+    elif len(sys.argv) > 2:
+        print('Usage is: python3 TemperatureWebService.py True/False. \n True/False is whether to engage the flask debugger')
+        sys.exit()
+    else:
+        debug = ''
+    app.run(debug=debug)
+    
